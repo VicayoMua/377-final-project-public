bd0d03678a344f0b30efc769cf34249e7b9f781e
Vicayo Chang <zhang@umass.edu>
2023-12-04 18:12:05 -0500

    11/17 3:23PM

--- src/main.cpp
+++ src/main.cpp
@@ -3,30 +3,50 @@
 
 int main() {
 
-    DiskVirtualMemoryManager *dvmm = new DiskVirtualMemoryManager("./mems");
-    ::printf("The disk folder can hold %lu new bytes. \n", dvmm->checkDiskFolderCapacity());
 
-    ::printf("Allocating 1 kilobyte as disk file ./mems/mem_0. \n");
-    DiskMemory dm0 = dvmm->malloc(1024);
+    DiskVirtualMemoryManager *dvmm = new DiskVirtualMemoryManager("mem_files");
 
-    printf("Printing hello to disk vm file ./mems/mem_0. \n");
-    void *page0 = dm0.pointer();
-    size_t page0_size = dm0.size();
-    snprintf((char *) page0, 7, "hello\n");
+    DiskMemory dm = dvmm->calloc(1024, sizeof(char));
 
-    printf("Asking machine to write changes to disk immediately and wait for completion. \n");
-    dm0.syncToDisk(true);
+    if (dm.pointer() != NULL) {
+        ::sprintf((char *) dm.pointer(), "Hellow World. ");
+    }else{
+        ::printf("NULL pointer");
+    }
 
-    printf("Freeing memory allocated for dm0. \n");
-    dvmm->free(dm0, false);
+    dm.syncToDisk(true);
 
-    ::printf("Allocating 1 kilobyte as disk vm file ./mems/mem_0. \n");
-    DiskMemory dm1 = dvmm->calloc(1024, 1);
-    dvmm->free(dm1, false);
+    dvmm->free(dm, false);
 
-    delete dvmm; // this operation will free all allocated space and delete memory files
+    delete dvmm;
 
     return 0;
 
+
+
+//    DiskVirtualMemoryManager *dvmm = new DiskVirtualMemoryManager("./mems");
+//    ::printf("The disk folder can hold %lu new bytes. \n", dvmm->checkDiskFolderCapacity());
+//
+//    ::printf("Allocating 1 kilobyte as disk file ./mems/mem_0. \n");
+//    DiskMemory dm0 = dvmm->malloc(1024);
+//
+//    printf("Printing hello to disk vm file ./mems/mem_0. \n");
+//    void *page0 = dm0.pointer();
+//    size_t page0_size = dm0.size();
+//    snprintf((char *) page0, 7, "hello\n");
+//
+//    printf("Asking machine to write changes to disk immediately and wait for completion. \n");
+//    dm0.syncToDisk(true);
+//
+//    printf("Freeing memory allocated for dm0. \n");
+//    dvmm->free(dm0, false);
+//
+//    ::printf("Allocating 1 kilobyte as disk vm file ./mems/mem_0. \n");
+//    DiskMemory dm1 = dvmm->calloc(1024, 1);
+//    dvmm->free(dm1, false);
+//
+//    delete dvmm; // this operation will free all allocated space and delete memory files
+
+    return 0;
 }
 
