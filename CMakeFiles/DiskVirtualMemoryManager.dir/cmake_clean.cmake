5e40888d784ad08c6d30f14bab83970950af58a2
Bach <bach.tlg03@gmail.com>
2023-12-06 18:22:33 -0500

    Added test

---
+++ CMakeFiles/DiskVirtualMemoryManager.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/DiskVirtualMemoryManager.dir/src/main.cpp.o"
+  "CMakeFiles/DiskVirtualMemoryManager.dir/src/main.cpp.o.d"
+  "DiskVirtualMemoryManager"
+  "DiskVirtualMemoryManager.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/DiskVirtualMemoryManager.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ CMakeFiles/TestCase.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/TestCase.dir/tests/test.cpp.o"
+  "CMakeFiles/TestCase.dir/tests/test.cpp.o.d"
+  "TestCase"
+  "TestCase.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/TestCase.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
